e181433c08c168c6fcb5ccbd3d706374d3b6aa25
Manuel Spigolon <behemoth89@gmail.com>
2019-01-09 23:13:41 +0100

    completed init command logic

--- commands/init.js
+++ commands/init.js
@@ -5,6 +5,7 @@ const { prompt } = require('enquirer')
 const { readFileSync } = require('fs')
 
 const parseArgs = require('../lib/args')
+const log = require('../lib/log')
 const CryptoStorage = require('../lib/CryptoStorage')
 
 module.exports = async function (args) {
@@ -13,40 +14,61 @@ module.exports = async function (args) {
     return showHelp()
   }
 
+  const storage = new CryptoStorage()
+  const exists = await storage.exists()
+
+  if (!opts.overwrite && exists) {
+    log.error('❌ Keepy-store already exists!')
+    return
+  }
+
   let initParameter = {
     name: '',
     password: '',
     reminder: ''
   }
 
-  if (opts.yes !== true) {
-    const question = [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Secure storage name?'
-      },
-      {
-        type: 'password',
-        name: 'password',
-        message: 'Whant to set a password?'
-      },
-      {
-        type: 'input',
-        name: 'reminder',
-        message: 'Whould you like to set a reminder for the password?'
-      }
-    ]
-
-    initParameter = await prompt(question)
-    console.log(initParameter)
+  if (opts.yes === true) {
+    initParameter.password = storage.randomPassword()
+  } else {
+    try {
+      initParameter = await askParameters()
+    } catch (error) {
+      log.error('Operation cancelled')
+      return
+    }
   }
 
-  const storage = new CryptoStorage()
-  storage.init(initParameter)
-  storage.persist()
+  try {
+    storage.init(initParameter)
+    await storage.persist()
+    log.info(`✨ Created keepy-store.json${opts.yes ? ` with password: ${initParameter.password}` : ''}`)
+  } catch (error) {
+    log.error(`Saving error: ${error.message}`)
+  }
 }
 
 function showHelp () {
   console.log(readFileSync(path.join(__dirname, '..', 'man', 'init.txt'), 'utf8'))
+}
+
+function askParameters () {
+  const question = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Secure storage name?'
+    },
+    {
+      type: 'password',
+      name: 'password',
+      message: 'Whant to set a password?'
+    },
+    {
+      type: 'input',
+      name: 'reminder',
+      message: 'Whould you like to set a reminder for the password?'
+    }
+  ]
+  return prompt(question)
 }
